5af89491dfb82b9003bd6a007fc3bf281264ad05
Evgeny Eroshin <eee333@yandex.ru>
2021-10-01 21:00:54 +0300

    Bugfix

--- app.py
+++ app.py
@@ -11,8 +11,10 @@ def index(book_id):
     with open('books.json', encoding="utf-8") as f:
         books = json.load(f)
         for book in books:
-            if book_id == book.id:
+            if book_id == book['id']:
                 return render_template("one_book.html", book=book)
+        return render_template("one_book.html", book={})
+
 
 
 if __name__ == '__main__':
