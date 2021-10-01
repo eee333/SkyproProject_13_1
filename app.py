54f49f59f174cd322de2ac7e0073776f6c04f086
Evgeny Eroshin <eee333@yandex.ru>
2021-10-01 20:33:21 +0300

    Add route for page one_book.html

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 # Small library. Home work 13.1 Создание API
 
+import json
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/<int:book_id>/')
+def index(book_id):
+    with open('books.json', encoding="utf-8") as f:
+        books = json.load(f)
+        for book in books:
+            if book_id == book.id:
+                return render_template("one_book.html", book=book)
