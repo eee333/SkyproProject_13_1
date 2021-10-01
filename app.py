a7ff19195e32989f8cb7e80610da78455c161515
Evgeny Eroshin <eee333@yandex.ru>
2021-10-02 00:18:34 +0300

    Create API. Torn of web interface.

--- app.py
+++ app.py
@@ -6,18 +6,27 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/<int:book_id>/')
+@app.route('/books/<int:book_id>/')
 def index(book_id):
     with open('books.json', encoding="utf-8") as f:
         books = json.load(f)
         for book in books:
             if book_id == book['id']:
-                return render_template("one_book.html", book=book)
-        return render_template("one_book.html", book={})
+                # return render_template("one_book.html", book=book)
+                return json.dumps(book, ensure_ascii=False)
+        # return render_template("one_book.html", book={})
+        return {}
+
+
+@app.route('/add', methods=['POST'])
+def add_book():
+    new_book = request.json
+    # return new_book
+    return "", 201
 
 
 @app.route('/add/')
-def add_book():
+def adding_book():
     return render_template("add_book.html")
 
 if __name__ == '__main__':
