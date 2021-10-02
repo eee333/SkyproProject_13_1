3084c0e7b25adb32448fd95f0b335c9e9d0db9de
Evgeny Eroshin <eee333@yandex.ru>
2021-10-02 18:21:14 +0300

    Bugfix

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def get_next_id(source_list, source_field):
 
 
 def get_next_isbn(source_list, source_field):
-    # 978-5-389-07435-5
+    # 978-5-389-07435-4
     next_isbn_int = 1
     for item in source_list:
         current_isbn = item[source_field]
@@ -25,7 +25,25 @@ def get_next_isbn(source_list, source_field):
             next_isbn_int = int(current_isbn)
     next_isbn_int += 1
     str_isbn = str(next_isbn_int)
-    next_isbn = ('{}-{}-{}-{}-{}'.format(str_isbn[0:3], str_isbn[3], str_isbn[4:7], str_isbn[7:12], str_isbn[12]))
+    isbn = [0, 0, 0, 0, 0]
+    isbn[0] = int(str_isbn[0:3])
+    isbn[1] = int(str_isbn[3])
+    isbn[2] = int(str_isbn[4:7])
+    isbn[3] = int(str_isbn[7:12])
+    isbn[4] = int(str_isbn[12])
+    if isbn[4] > 4:
+        isbn[4] = 0
+        isbn[3] += 1
+    if isbn[3] > 9999:
+        isbn[3] = 0
+        isbn[2] += 1
+    if isbn[2] > 999:
+        isbn[2] = 0
+        isbn[1] += 1
+    if isbn[1] > 6:
+        isbn[1] = 0
+        isbn[0] += 1
+    next_isbn = ('{:03d}-{}-{:03d}-{:05d}-{}'.format(isbn[0], isbn[1], isbn[2], isbn[3], isbn[4]))
     return next_isbn
 
 
